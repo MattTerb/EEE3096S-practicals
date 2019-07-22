34269af65acf558955c8fa8779b96d28c575b467
MattTerb <mterblanche13@gmail.com>
2019-07-22 21:12:34 +0200

    Turn LED on and off

--- template.py
+++ template.py
@@ -3,21 +3,30 @@
 Python Practical Template
 Keegan Crankshaw
 Readjust this Docstring as follows:
-Names: <names>
-Student Number: <studnum>
-Prac: <Prac Num>
-Date: <dd/mm/yyyy>
+Names: Mattthew Terblanche
+Student Number: TRBMAT002
+Prac: 1
+Date: 22/07/2019
 """
 
 # import Relevant Librares
 import RPi.GPIO as GPIO
+import time
 
 # Logic that you write
 def main():
-    print("write your logic here")
+
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setup(17, GPIO.OUT)
+    GPIO.output(17, True)
+    time.sleep(5)
+    GPIO.output(17, False)
+    time.sleep(5)
 
 
-# Only run the functions if 
+
+
+# Only run the functions if
 if __name__ == "__main__":
     # Make sure the GPIO is stopped correctly
     try:
