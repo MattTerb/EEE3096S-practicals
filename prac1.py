62d999d6224ac73f897ee6ebbb59b2df50dfc57a
MattTerb <mterblanche13@gmail.com>
2019-07-23 17:45:39 +0200

    Turn LED on/off with button

--- prac1.py
+++ prac1.py
@@ -15,21 +15,25 @@ import time
 
 # Logic that you write
 
+LED_status = 0		# Create variable for LED status and set it to off
 def setup():
 
     GPIO.setmode(GPIO.BCM)		# Set numbering system to BCM
     GPIO.setup(17, GPIO.OUT)		# Set LED on pin 17 to output
+    GPIO.setup(23, GPIO.IN)		# Set button on pin 23 to input
+    GPIO.output(17, LED_status)		# Set LED to off
 
+def switchLED(self):
+    global LED_status
+    LED_status = not LED_status		# Turn LED on if off or off if on
+    GPIO.output(17, LED_status)		# Turn LED on
 
 def main():
 
-    GPIO.output(17, True)		# Turn LED on
-    time.sleep(2)			# Wait for 5 seconds
-    GPIO.output(17, False)		# Turn LED off
-    time.sleep(2)
+    GPIO.add_event_detect(23, GPIO.RISING, callback=switchLED, bouncetime=200)		# Wait for button press
 
-
-
+    while True:
+        time.sleep(1)
 
 # Only run the functions if
 if __name__ == "__main__":
